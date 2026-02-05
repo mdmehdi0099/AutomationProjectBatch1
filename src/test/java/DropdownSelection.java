15a278a99b64a7d2bbcb02037d2cd7fc89ce88fe
mdmehdi0099 <mdmehdi0099@gmail.com>
2026-02-05 21:43:52 +0530

    updating the code of alert

--- src/test/java/DropdownSelection.java
+++ src/test/java/DropdownSelection.java
@@ -1,6 +1,4 @@
-import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
+import org.openqa.selenium.*;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.ui.*;
@@ -13,13 +11,57 @@ public class DropdownSelection {
         driver.get("https://www.orangehrm.com/");
         driver.manage().window().maximize();
         Thread.sleep(2000);
-        driver.findElement(By.xpath("//select[@name='locale']")).click();
 
+
+        /*
+        driver.findElement(By.xpath("//select[@name='locale']")).click();
         WebElement dropdownElement=driver.findElement(By.xpath("//select[@name='locale']"));
 
         Select newSelect=new Select(dropdownElement);
-        newSelect.selectByIndex(1);
+        //newSelect.selectByIndex(1);
+        newSelect.selectByValue("/es");
         Thread.sleep(3000);
+
+         */
+
+
+        //select the option by keyboard
+        driver.findElement(By.xpath("//select[@name='locale']")).click();
+        Thread.sleep(2000);
+        driver.findElement(By.xpath("//select[@name='locale']")).sendKeys(Keys.ARROW_DOWN);
+        Thread.sleep(2000);
+        driver.findElement(By.xpath("//select[@name='locale']")).sendKeys(Keys.ENTER);
+
+        Thread.sleep(3000);
+
+        //Handling Alerts in selenium
+        //Text,Accept(OK),Deny(cancel)
+        Alert alert=driver.switchTo().alert();
+        alert.accept();//ok
+        alert.dismiss();//cancel
+
+
+        alert.sendKeys("ok please proceed");//to enter any text in alert
+
+        //get text of alert
+        String alertText=alert.getText();
+        if (alertText.equalsIgnoreCase("Success")){
+            System.out.println("Successfully submitted");
+        }else{
+            System.out.println("Failure alert message");
+        }
+
+
+
+
+
+
+
+
+
         driver.quit();
+
+
+
     }
 }
