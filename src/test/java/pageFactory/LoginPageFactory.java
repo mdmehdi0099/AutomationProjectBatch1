fbbab71f5b6787fd8c411d882acb5a58ee339768
mdmehdi0099 <mdmehdi0099@gmail.com>
2026-02-28 22:26:36 +0530

    wait implementation in page factory 28 Feb2026

--- src/test/java/pageFactory/LoginPageFactory.java
+++ src/test/java/pageFactory/LoginPageFactory.java
@@ -1,28 +1,41 @@
 package pageFactory;
 
 import hooks.BaseClass;
+import org.junit.rules.ExpectedException;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.ExpectedCondition;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
 
 public class LoginPageFactory{
     WebDriver driver;
+    //explicit wait listener
+    WebDriverWait wait;
+
     public LoginPageFactory(WebDriver driver){
         this.driver=driver;
         PageFactory.initElements(driver,this);
+        this.wait=new WebDriverWait(driver,Duration.ofSeconds(300));
     }
 
     @FindBy(xpath = "(//a[contains(text(),'Sign in')])[1]")
     WebElement signIn;
 
     public void clickOnSign(){
+        wait.until(ExpectedConditions.elementToBeClickable(signIn));
         signIn.click();
     }
     @FindBy(xpath = "//input[@id='username']")
     WebElement username;
 
     public void enterUsername(String Username){
+        wait.until(ExpectedConditions.elementToBeClickable(username));
         username.sendKeys(Username);
     }
 
@@ -30,6 +43,7 @@ public class LoginPageFactory{
     WebElement password;
 
     public void enterPassword(String Password){
+        wait.until(ExpectedConditions.elementToBeClickable(password));
         password.sendKeys(Password);
     }
 
@@ -37,11 +51,9 @@ public class LoginPageFactory{
     WebElement loginBtn;
 
     public void clickOnLoginBtn(){
+        wait.until(ExpectedConditions.elementToBeClickable(loginBtn));
         loginBtn.click();
     }
-
-
-
 
 
 }
--- src/test/java/stepDefinitions/LoginStepDefinition.java
+++ src/test/java/stepDefinitions/LoginStepDefinition.java
@@ -1,5 +1,7 @@
 package stepDefinitions;
 
+import io.cucumber.java.After;
+import io.cucumber.java.Before;
 import io.cucumber.java.en.And;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
@@ -17,39 +19,47 @@ public class LoginStepDefinition {
     //chrome driver initialization
     WebDriver driver;
     LoginPageFactory loginPageFactory;
+    @After
+    public void tearDown(){
+        System.out.println("@After");
+        driver.quit();
+    }
+    @Before
+    public void setup(){
+        driver=new ChromeDriver();
+        loginPageFactory=new LoginPageFactory(driver);
+        System.out.println("@Before");
+    }
 
     @Given("The user is on the linkedIn {string} page")
     public void the_user_is_on_the_linked_in_page(String url) throws InterruptedException {
-        driver=new ChromeDriver();
-        loginPageFactory=new LoginPageFactory(driver);
         //go to the linkedIn Url
         //String url= GlobalConfig.get("linkedInURL");
         driver.get(url);
         driver.manage().window().maximize();
-        Thread.sleep(2000);
+        //Thread.sleep(2000);
         //click on signIn button
         loginPageFactory.clickOnSign();
-
     }
     @When("The user enters the username {string} and password {string}")
     public void the_user_enters_the_username_and_password(String username, String password) throws InterruptedException {
-        Thread.sleep(2000);
+        //Thread.sleep(2000);
         loginPageFactory.enterUsername(username);
-
-        Thread.sleep(2000);
+        //Thread.sleep(2000);
         loginPageFactory.enterPassword(password);
     }
     @When("The user will click on the login button")
     public void the_user_will_click_on_the_login_button() throws InterruptedException {
-        Thread.sleep(2000);
+        //Thread.sleep(2000);
         //click on login btn
         loginPageFactory.clickOnLoginBtn();
     }
     @Then("The user should be redirected to the Home Page {string}")
     public void the_user_should_be_redirected_to_the_home_page(String string) {
         System.out.println("The user is on the homepage");
-        driver.quit();
+
     }
+
 
 
 
