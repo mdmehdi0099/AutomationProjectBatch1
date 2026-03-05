414e951d1229c4c7e01f163f877ce5daeef1fb64
mdmehdi0099 <mdmehdi0099@gmail.com>
2026-03-06 00:46:35 +0530

    updating the logger code

--- src/test/java/stepDefinitions/LoginStepDefinition.java
+++ src/test/java/stepDefinitions/LoginStepDefinition.java
@@ -6,20 +6,20 @@ import io.cucumber.java.en.And;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
+import lombok.extern.slf4j.Slf4j;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import pageFactory.LoginPageFactory;
 import utility.GlobalConfig;
-
-import java.util.logging.LogManager;
 import java.util.logging.Logger;
 
 
 public class LoginStepDefinition {
-    //private static Logger logger= LogManager.getLogManager().getLogger(String.valueOf(LoginStepDefinition.class));
+    private static final Logger logger= Logger.getLogger(LoginStepDefinition.class.getName());
     //chrome driver initialization
+
     WebDriver driver;
     LoginPageFactory loginPageFactory;
     @After
@@ -67,31 +67,31 @@ public class LoginStepDefinition {
     @Given("The user is on the amazon site {string}")
     public void the_user_is_on_the_amazon_site(String string) {
         //System.out.println("The user is on the amazon site {string}");
-        //logger.info("The user is on the amazon site {string}");
+        logger.info("The user is on the amazon site");
     }
     @Given("The user will enter the username {string} and password {string} on the amazon login page")
     public void the_user_will_enter_the_username_and_password_on_the_amazon_login_page(String string, String string2) {
-        System.out.println("The user will enter the username {string} and password {string} on the amazon login page");
-        //logger.info("The user will enter the username {string} and password {string} on the amazon login page");
+        //System.out.println("The user will enter the username {string} and password {string} on the amazon login page");
+        logger.info("The user will enter the username  and password  on the amazon login page");
     }
     @Given("The user click on the login button")
     public void the_user_click_on_the_login_button() {
-        System.out.println("The user click on the login button");
-        //logger.info("The user click on the login button");
+        //System.out.println("The user click on the login button");
+        logger.info("The user click on the login button");
 
     }
     @Given("The user will search the item {string} on the amazon")
     public void the_user_will_search_the_item_on_the_amazon(String string) {
-        System.out.println("The user will search the item {string} on the amazon");
+        logger.info("The user will search the item  on the amazon");
     }
     @When("The user will add the product on cart")
     public void the_user_will_add_the_product_on_cart() {
-        System.out.println("The user will add the product on cart");
+        logger.info("The user will add the product on cart");
 
     }
     @Then("The product should be visible on the cart")
     public void the_product_should_be_visible_on_the_cart() {
-        System.out.println("The product should be visible on the cart");
+        logger.info("The product should be visible on the cart");
     }
 
 
